6879a0f7c6e539e194a04cc1e9e7c30c82498871
Anish4u <anish.sasidharan4u@gmail.com>
2023-12-14 22:34:13 +0200

    formatted code

--- src/data/inMemoryStorage.ts
+++ src/data/inMemoryStorage.ts
@@ -1,20 +1,12 @@
 // Import the IoTData model
 import { IoTData } from "../app/models/dataModel";
 
-// Define the structure of processed data
-interface ProcessedData {
-  averageTemperature: number;
-  averageHumidity: number;
-  timestamp: Date;
-}
+
 
 // Class to store and manage in-memory data
 class InMemoryStorage {
   // Raw IoT data storage
   private data: IoTData[] = [];
-
-  // Processed data storage
-  private processedData: ProcessedData[] = [];
 
   // Method to store raw IoT data
   storeData(data: IoTData): void {
